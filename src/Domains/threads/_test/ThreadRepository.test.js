0dcc1ff65d183ad7c8ede267ca441088f8540524
hakimamarullah <hakimamarullah@gmail.com>
2022-12-16 16:00:34 +0700

    Feat:setup thread development

---
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -0,0 +1,10 @@
+const ThreadRepository = require('../ThreadRepository');
+
+describe('ThreadRepository interface', () => {
+  it('should throw error when invoke unimplemented method', async () => {
+    const threadRepository = new ThreadRepository();
+
+    await expect(threadRepository.addThread('', '')).rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(threadRepository.deleteThread('', '')).rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
---
+++ src/Domains/threads/entities/_test/AddedThread.test.js
@@ -0,0 +1,42 @@
+const AddedThread = require('../AddedThread');
+
+describe('a AddedThread entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      title: 'dicoding',
+    };
+
+    // Action and Assert
+    expect(() => new AddedThread(payload)).toThrowError('ADDED_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      id: 123,
+      title: 'dicoding',
+      owner: {},
+    };
+
+    // Action and Assert
+    expect(() => new AddedThread(payload)).toThrowError('ADDED_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create addedThread object correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'user-123',
+      title: 'dicoding',
+      owner: 'user-1',
+    };
+
+    // Action
+    const addedThread = new AddedThread(payload);
+
+    // Assert
+    expect(addedThread.id).toEqual(payload.id);
+    expect(addedThread.title).toEqual(payload.title);
+    expect(addedThread.owner).toEqual(payload.owner);
+  });
+});
